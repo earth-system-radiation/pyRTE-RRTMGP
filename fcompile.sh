13517f0653fff853c643a4a203a3ff78aed170e1
Alex Soklev <alex@makepath.com>
2024-03-09 19:21:45 +0200

    Integrate CMake with setup.py to automate building of bindings across different platforms.
    Tested to work on MacOS

--- fcompile.sh
+++ fcompile.sh
@@ -9,7 +9,7 @@ echo "Linking"
 # Create a shared library from the fortran code
 gfortran -shared -o libtest.so test.o
 # Build an executable program from the fortran code
-gfortran -o fortran_test.bin test.o
+#gfortran -o fortran_test.bin test.o
 
 # Print what functions are visible in the dll
 nm -g -C --defined-only libtest.so 
--- setup.py
+++ setup.py
@@ -1,21 +1,83 @@
-from setuptools import setup, Extension
-import pybind11
+import os
+import re
+import sys
+import platform
+import subprocess
 
-ext_modules = [
-    Extension(
-        'pybind_interface',
-        ['pybind_interface.cpp'],
-        include_dirs=[pybind11.get_include()],
-        libraries=['test'],  # This should match the name of your compiled C++ library
-        library_dirs=['./build'],  # This should point to the directory where libtest.so is located
-        extra_compile_args=['-std=c++11'],
-        extra_link_args=['-Wl,-rpath=./build'],  # This ensures that the shared library can be found during runtime
-    ),
-]
+from packaging.version import Version
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from shutil import copyfile
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+        self.moduleName = name
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+
+        cmake_version = Version(re.search(r'version\s*([\d.]+)',
+                                out.decode()).group(1))
+        print(f"CMake Version: {cmake_version}")
+        if cmake_version < Version('3.18.0'):
+            raise RuntimeError("CMake >= 3.18.0 is required")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+        extdir += '/' + ext.moduleName
+        if platform.system() == "Windows":
+            extdir = extdir.replace("/","\\")
+
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir]
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+        conda_build = os.environ.get("CONDA_BUILD", 0)
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(),
+                extdir)]
+            # CMake lets you override the generator, as is done in conda build.
+            # If using NMake for generator or building on conda, do not use
+            # arch specifier as not supported.
+            cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+            if sys.maxsize > 2**32 and not (
+                    cmake_generator.startswith("NMake") or conda_build):
+                cmake_args += ['-A', 'x64']
+            if not conda_build:
+                build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
+        if platform.system() == "Windows":
+            src = self.build_temp + "\\" + cfg + "\\" + ext.moduleName + ".dll"
+            dst = extdir + "\\" + ext.moduleName + ".dll"
+            copyfile(src, dst)
 
 setup(
     version='0.0.1',
-    name='pybind_interface',
-    ext_modules=ext_modules,
-    script_args=['build_ext', '--build-lib', './build'],  # Specify the build directory here
+    ext_modules=[CMakeExtension('rttpy')],
+    cmdclass=dict(build_ext=CMakeBuild),
 )
