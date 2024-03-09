bacec3a24914006ec0cf12b25750c5be41f74872
Alex Soklev <alex@makepath.com>
2024-03-09 18:50:17 +0200

    Cleanup fcompile.sh. Do not compile C++. Cmake does that now

--- fcompile.sh
+++ fcompile.sh
@@ -13,9 +13,3 @@ gfortran -o fortran_test.bin test.o
 
 # Print what functions are visible in the dll
 nm -g -C --defined-only libtest.so 
-
-# Build example C program using the fortran functions
-g++ -L${PWD} -ltest -o c_test.bin ../main.cpp 
-
-# Build C example with debug information
-#g++ -g -O0 -L${PWD} -ltest -o c_test.bin ../main.cpp 
