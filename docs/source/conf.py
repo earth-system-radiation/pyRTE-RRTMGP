2f845dedffc50b8ffc2bd1d4dabb6bcbba0d7a4b
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2024-03-29 10:26:10 -0700

    Read version from toml file to display in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,25 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import datetime as dt
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+import tomllib
 
-import datetime as dt
+sys.path.insert(0, os.path.abspath("../../"))
+
+
+def get_version_from_toml():
+    if os.path.isfile("../../pyproject.toml"):
+        # read pyproject.toml
+        with open("../../pyproject.toml", "rb") as f:
+            pyproject = tomllib.load(f)
+        # get version from pyproject.toml
+        version = pyproject.get("project", {}).get("version", {})
+    else:
+        version = None
+    return version if version else "dev"
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -15,7 +29,8 @@ import datetime as dt
 project = "pyRTE-RRTMGP"
 copyright = f"{dt.datetime.now().year}, Atmospheric and Environmental Research"
 author = "Atmospheric and Environmental Research"
-release = "0.1"
+version = get_version_from_toml()
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -36,9 +51,12 @@ autodoc_mock_imports = [
     "xarray",
 ]
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "display_version": True,
+}
