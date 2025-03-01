b6eb2c073e532b90bf2662a040d2d5921d9036f5
Josué Sehnem <josue@sehnem.com>
2025-03-01 14:05:30 -0300

    define all data files in enums

--- pyrte_rrtmgp/data_types.py
+++ pyrte_rrtmgp/data_types.py
@@ -23,6 +23,85 @@ class GasOpticsFiles(StrEnum):
     SW_G224 = "rrtmgp-gas-sw-g224.nc"
 
 
+class CloudOpticsFiles(StrEnum):
+    """Enumeration of default RRTMGP cloud optics data files.
+
+    This enum defines the available pre-configured cloud optics data files that can be
+    used with RRTMGP. The files contain cloud optical properties needed for radiative
+    transfer calculations.
+
+    Attributes:
+        LW_BND: Longwave cloud optics file with band points
+        LW_128: Longwave cloud optics file with 128 g-points
+        LW_256: Longwave cloud optics file with 256 g-points
+        SW_BND: Shortwave cloud optics file with band points
+        SW_112: Shortwave cloud optics file with 112 g-points
+        SW_224: Shortwave cloud optics file with 224 g-points
+    """
+
+    LW_BND = "rrtmgp-clouds-lw-bnd.nc"
+    LW_G128 = "rrtmgp-clouds-lw-g128.nc"
+    LW_G256 = "rrtmgp-clouds-lw-g256.nc"
+    SW_BND = "rrtmgp-clouds-sw-bnd.nc"
+    SW_G112 = "rrtmgp-clouds-sw-g112.nc"
+    SW_G224 = "rrtmgp-clouds-sw-g224.nc"
+
+
+class AerosolOpticsFiles(StrEnum):
+    """Enumeration of default RRTMGP aerosol optics data files.
+
+    This enum defines the available pre-configured aerosol optics data files that can be
+    used with RRTMGP. The files contain aerosol optical properties needed for radiative
+    transfer calculations.
+    """
+
+    LW_MERRA = "rrtmgp-aerosols-merra-lw.nc"
+    SW_MERRA = "rrtmgp-aerosols-merra-sw.nc"
+
+
+class RFMIPExampleFiles(StrEnum):
+    """Enumeration of default RFMIP example files.
+
+    This enum defines the available pre-configured example files that can be used with
+    RRTMGP. The files contain example data for radiative transfer calculations.
+    """
+
+    INPUT = (
+        "examples/rfmip-clear-sky/inputs/"
+        "multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc"
+    )
+    REFERENCE_RLD = (
+        "examples/rfmip-clear-sky/reference/"
+        "rld_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc"
+    )
+    REFERENCE_RLU = (
+        "examples/rfmip-clear-sky/reference/"
+        "rlu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc"
+    )
+    REFERENCE_RSD = (
+        "examples/rfmip-clear-sky/reference/"
+        "rsd_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc"
+    )
+    REFERENCE_RSU = (
+        "examples/rfmip-clear-sky/reference/"
+        "rsu_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc"
+    )
+
+
+class AllSkyExampleFiles(StrEnum):
+    """Enumeration of default all-sky example files.
+
+    This enum defines the available pre-configured all-sky example files that can be
+    used with RRTMGP. The files contain example data for radiative transfer
+    calculations.
+    """
+
+    LW_NO_AEROSOL = "examples/all-sky/reference/rrtmgp-allsky-lw-no-aerosols.nc"
+    LW = "examples/all-sky/reference/rrtmgp-allsky-lw.nc"
+    SW_NO_AEROSOL = "examples/all-sky/reference/rrtmgp-allsky-sw-no-aerosols.nc"
+    SW = "examples/all-sky/reference/rrtmgp-allsky-sw.nc"
+
+
 class ProblemTypes(StrEnum):
     """Enumeration of available radiation calculation types.
 
