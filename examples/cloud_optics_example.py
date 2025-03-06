6fcd31b8f1deb9931f6e32c54500b8739c25eeb0
Robert Pincus <Robert.Pincus@columbia.edu>
2025-03-06 12:17:08 -0500

    Pre-commit formatting

--- examples/cloud_optics_example.py
+++ examples/cloud_optics_example.py
@@ -1,25 +1,36 @@
-#! /usr/env/python 
-import xarray as xr
+#! /usr/env/python
 import scipy as sc
+import xarray as xr
 
 #
-# Compute cloud optics for a large sample of clouds 
-#   fetch-DYAMOND-data.py must be run first. 
+# Compute cloud optics for a large sample of clouds
+#   fetch-DYAMOND-data.py must be run first.
 #
 
 #
 # Only about half the levels contain clouds so for cloud optics only we can limit the data we import
 #
-min_lev_liquid = 107 # There are almost no liquid clouds in levels higher (smaller index is higher) than 107
-min_lev_ice = 78 # There are almost no ice clouds in levels higher (smaller index is higher) than 78
+min_lev_liquid = 107  # There are almost no liquid clouds in levels higher (smaller index is higher) than 107
+min_lev_ice = 78  # There are almost no ice clouds in levels higher (smaller index is higher) than 78
 
-atmosphere= xr.open_mfdataset(
-	"GEOS-DYAMOND2-data/*.nc4", 
-	drop_variables=["anchor", "cubed_sphere", "orientation", "contacts", "corner_lats", "corner_lons"]
-).isel(lev=slice(min_lev_ice,))
+atmosphere = xr.open_mfdataset(
+    "GEOS-DYAMOND2-data/*.nc4",
+    drop_variables=[
+        "anchor",
+        "cubed_sphere",
+        "orientation",
+        "contacts",
+        "corner_lats",
+        "corner_lons",
+    ],
+).isel(
+    lev=slice(
+        min_lev_ice,
+    )
+)
 #
 # By default the dask arrays are contiguous in Xdim and Ydim
-#   This will work for computing optics but not for computing fluxes, 
+#   This will work for computing optics but not for computing fluxes,
 #   where chunks need to include all layers
 #
 
@@ -27,6 +38,6 @@ atmosphere["LWP"] = (atmosphere.DELP * atmosphere.QL) / sc.constants.g
 atmosphere["IWP"] = (atmosphere.DELP * atmosphere.QI) / sc.constants.g
 
 #
-# What we'd eventutally like to do... 
+# What we'd eventutally like to do...
 #
 clouds_optical_props = cloud_optics_sw.compute_cloud_optics(atmosphere)
--- examples/fetch-DYAMOND-data.py
+++ examples/fetch-DYAMOND-data.py
@@ -1,35 +1,36 @@
-#! /usr/env/python 
+#! /usr/env/python
 
 #
-# Download sample data on which to exercise pyRTE. 
-#   Data comes from the NASA/GMAO contribution to DYAMOND2 described at 
+# Download sample data on which to exercise pyRTE.
+#   Data comes from the NASA/GMAO contribution to DYAMOND2 described at
 #   https://gmao.gsfc.nasa.gov/global_mesoscale/dyamond_phaseII/data_access/
 #   Data is on a cubed sphere at resolution c2880 (~3.5 km globally, or about 50 million columns)
 #   Files are about 6Gb per snapshot (we choose Feb 1 at 9 Z); files for cloud variables are smaller
 #
 
 urls = [
-  f"https://portal.nccs.nasa.gov/datashare/G5NR/DYAMONDv2/03KM/DYAMONDv2_c2880_L181/inst_01hr_3d_{v}_Mv/202002/DYAMONDv2_c2880_L181.inst_01hr_3d_{v}_Mv.20200201_0900z.nc4"
-  for v in ["QL", "QI", "RL", "RI", "DELP", "CO2", "QV", "T", "P"]
+    f"https://portal.nccs.nasa.gov/datashare/G5NR/DYAMONDv2/03KM/DYAMONDv2_c2880_L181/inst_01hr_3d_{v}_Mv/202002/DYAMONDv2_c2880_L181.inst_01hr_3d_{v}_Mv.20200201_0900z.nc4"
+    for v in ["QL", "QI", "RL", "RI", "DELP", "CO2", "QV", "T", "P"]
 ]
 #
 # Ozone is 6-hourly
 #
-urls.append(["https://portal.nccs.nasa.gov/datashare/G5NR/DYAMONDv2/03KM/DYAMONDv2_c2880_L181/geosgcm_prog/202002/DYAMONDv2_c2880_L181.geosgcm_prog.20200201_0600z.nc4"])
+urls.append(
+    [
+        "https://portal.nccs.nasa.gov/datashare/G5NR/DYAMONDv2/03KM/DYAMONDv2_c2880_L181/geosgcm_prog/202002/DYAMONDv2_c2880_L181.geosgcm_prog.20200201_0600z.nc4"
+    ]
+)
 
 
-import pooch 
-from pathlib import Path 
+from pathlib import Path
+
+import pooch
 
 data_dir = "GEOS-DYAMOND2-data"
 with open("registry.txt", "w") as registry:
-  for u in urls: 
-    fname=Path(u).parts[-1]
-    # Download each data file to the specified directory
-    path = pooch.retrieve(
-        url=u, known_hash=None, fname=fname, path=data_dir
-    )
-    # Add the name, hash, and url of the file to the new registry file
-    registry.write(
-        f"{fname} {pooch.file_hash(path)} {u}\n"
-    )
+    for u in urls:
+        fname = Path(u).parts[-1]
+        # Download each data file to the specified directory
+        path = pooch.retrieve(url=u, known_hash=None, fname=fname, path=data_dir)
+        # Add the name, hash, and url of the file to the new registry file
+        registry.write(f"{fname} {pooch.file_hash(path)} {u}\n")
