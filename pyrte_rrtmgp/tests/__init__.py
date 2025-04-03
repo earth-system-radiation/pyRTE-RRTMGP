9cbc9ac74806a7c062acda2dd9c368b24d5f65cb
brendancol <brendancol@gmail.com>
2025-04-02 22:32:23 -0400

    added test for dask sw_solver_noscat

---
+++ pyrte_rrtmgp/tests/__init__.py
@@ -0,0 +1,24 @@
+
+DEFAULT_GAS_MAPPING = {
+    "h2o": "water_vapor",
+    "co2": "carbon_dioxide_GM",
+    "o3": "ozone",
+    "n2o": "nitrous_oxide_GM",
+    "co": "carbon_monoxide_GM",
+    "ch4": "methane_GM",
+    "o2": "oxygen_GM",
+    "n2": "nitrogen_GM",
+    "ccl4": "carbon_tetrachloride_GM",
+    "cfc11": "cfc11_GM",
+    "cfc12": "cfc12_GM",
+    "cfc22": "hcfc22_GM",
+    "hfc143a": "hfc143a_GM",
+    "hfc125": "hfc125_GM",
+    "hfc23": "hfc23_GM",
+    "hfc32": "hfc32_GM",
+    "hfc134a": "hfc134a_GM",
+    "cf4": "cf4_GM",
+    "no2": "no2",
+}
+
+ERROR_TOLERANCE = 1e-7
