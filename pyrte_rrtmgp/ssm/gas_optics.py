ec4e83d51888ff97d2a57ca6f6793016016f1e05
Sarika de Bruyn <156474534+sarika-debruyn@users.noreply.github.com>
2026-04-08 13:46:02 -0500

    Clarify imports and functionality in gas_optics.py
    Added a comment to clarify imports and functionality.

--- pyrte_rrtmgp/ssm/gas_optics.py
+++ pyrte_rrtmgp/ssm/gas_optics.py
@@ -1,3 +1,4 @@
 # contains the GasOptics class
 # Provides the interface (what the user actually calls and what they get back and gets plugged into rest of pyRTE)
+# Imports functions from kernel.py and calls them from inside compute() 
 
