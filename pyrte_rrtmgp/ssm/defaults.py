680dd38c579f497305f11766b93fa38c63c1af93
Sarika de Bruyn <sarika.debruyn@gmail.com>
2026-04-09 11:17:27 -0400

    finished defaults

--- pyrte_rrtmgp/ssm/defaults.py
+++ pyrte_rrtmgp/ssm/defaults.py
@@ -3,56 +3,61 @@
 
 import numpy as np
 
-Tsun_ssm = 5760.0 #default sun temeprature for SSM
-tsi = 1360.0 #default total solar irradiance
+TSUN_SSM = 5760.0 #default sun temeprature for SSM
+TSI = 1360.0 #default total solar irradiance
 
 #Molecular weights (kg/mol)
-mw_h2o = 0.018
-mw_co2 = 0.044
-mw_03 = 0.048
-m_dry = 0.029 #dry air
+MW_H2O = 0.018
+MW_CO2 = 0.044
+MW_O3 = 0.048
+M_DRY = 0.029 #dry air
 
 # default cloud absorption coefficients (m2/kg)
-kappa_cld_lw = 50.0
-kappa_cld_sw == 0.0001
+KAPPA_CLD_LW = 50.0
+KAPPA_CLD_SW = 0.0001
 
 # default cloud single scattering albedo
-ssa_cld_lw = 0.0
-ssa_cld_sw = 0.9999
+SSA_CLD_LW = 0.0
+SSA_CLD_SW = 0.9999
 
 # default cloud asymmetry
-g_cld_lw = 0.0
-g_cld_sw = 0.85
+G_CLD_LW = 0.0
+G_CLD_SW = 0.85
 
 # default nnu
-nnu_def = 41
+NNU_DEF = 41
 
 # wavelength ranges (cm^-1)
-nu_min_lw_def = 0.0
-nu_max_lw_def = 3500.0
+NU_MIN_LW_DEF = 0.0
+NU_MAX_LW_DEF = 3500.0
 
-nu_min_sw_def = 0.0
-nu_max_sw_def = 50000.0
+NU_MIN_SW_DEF = 0.0
+NU_MAX_SW_DEF = 50000.0
 
 # default wavenumber arrays
-nus_lw_def = np.linspace(50.0, 3000.0, nnu_def)
-nus_sw_def = np.linspace(1000.0, 45000.0, nnu_def)
+NUS_LW_DEF = np.linspace(50.0, 3000.0, NNU_DEF)
+NUS_SW_DEF = np.linspace(1000.0, 45000.0, NNU_DEF)
 
 # default spectroscopic params
-triangle_params_def_lw = np.array([
+TRIANGLE_PARAMS_DEF_LW = np.array([
   [1., 282., 0., 64.],
   [1., 24., 1600., 52.],
   [2., 110., 667., 12.]
 ])
 
-gas_names_def_lw = ["h2o", "co2"]
+GAS_NAMES_DEF_LW = ["h2o", "co2"]
 
-triangle_params_def_sw = np.array([
+TRIANGLE_PARAMS_DEF_SW = np.array([
   [1., 1., 0., 1200.],
   [2., 0., 0., 1000000.]
 ]) 
 
-gas_names_def_sw = ["h2o", "o3"]
+GAS_NAMES_DEF_SW = ["h2o", "o3"]
 
+P_REF = 500.0 * 100.0 # reference pressure hPa -> Pa
 
-
+MOL_WEIGHTS = {
+  "h2o": MW_H2O,
+  "co2": MW_CO2,
+  "o3": MW_O3,
+}
