7b47cb298040a0a69850ff21bf37ef0d757b74f7
Robert Pincus <Robert.Pincus@columbia.edu>
2026-06-05 16:25:14 -0400

    Molar masses from Python package

--- pyrte_rrtmgp/utils.py
+++ pyrte_rrtmgp/utils.py
@@ -1,10 +1,33 @@
 """Constants and functions for radiative transfer and gas optics calculations."""
 
+from typing import Dict, Final
+
 import numpy as np
+from molmass import Formula
 from numba import float64, vectorize
 from scipy.constants import Boltzmann as k_B
 from scipy.constants import h
 from scipy.constants import speed_of_light as c
+
+#
+# Chemical formulae for gases known to RRTMGP that aren't clear from the name
+#   Formulae from Wikipedia
+#
+halocarbons: Final[Dict[str, str]] = {
+    "ccl4": "CCl4",
+    "cfc11": "CCl3F",
+    "cfc12": "CCl2F2",
+    "cfc22": "CHClF2",
+    "hfc23": "CHF3",
+    "hfc32": "CH2F2",
+    "hfc125": "C2HF5",
+    "hfc143a": "C2H3F3",
+}
+#
+# International standard atmosphere:
+#  https://en.wikipedia.org/wiki/International_Standard_Atmosphere
+#
+M_DRY: Final[float64] = 28.966e-3
 
 
 #######################
@@ -43,3 +66,13 @@ def Tb_nu(B: float64, nu: float64) -> float64:
     """
     nu = nu * 100
     return (h * c * nu) / k_B * 1 / np.log((2 * h * (c**2) * nu**3) * 100.0 / B + 1)
+
+
+###
+#
+# Molar mass of gases
+#
+def get_molmass(gas_name: str) -> float64:
+    """Molar mass in MKS units."""
+    f = halocarbons[gas_name] if gas_name in halocarbons.keys() else gas_name.upper()
+    return Formula(f).mass * 1.0e-3
