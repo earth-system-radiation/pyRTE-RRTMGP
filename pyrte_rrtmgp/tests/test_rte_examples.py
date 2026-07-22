4ed0a4e5fa7a8ada2fce711ffe1266608ab7ce78
Robert Pincus <Robert.Pincus@columbia.edu>
2026-07-22 14:26:15 -0400

    Generate rte_examples testing combos programatically

--- pyrte_rrtmgp/tests/test_rte_examples.py
+++ pyrte_rrtmgp/tests/test_rte_examples.py
@@ -5,7 +5,7 @@ import pytest
 from typing import Dict, Optional, Any
 
 from pyrte_rrtmgp.rrtmgp.data_files import GasOpticsFiles
-from pyrte_rrtmgp.rrtmgp.examples import RTE_EXAMPLES
+from pyrte_rrtmgp.rrtmgp.examples   import RTE_EXAMPLES
 
 from pyrte_rrtmgp.rte_examples import (
     load_rte_example_file,
@@ -43,34 +43,16 @@ def _test_get_fluxes_from_rte_example(
 #
 #### Tests start here
 #
-# Better to build the testing variants (combos of state and gas optics)
-#    programatically
-#
 # pyRRTMGP LW GasOptics fails using 128 g-points
 #
 # Should add tests against reference answers
 #
 @pytest.mark.parametrize(
     "example, gas_optics",
-    [
-        ("RCE_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("RCE_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
-        ("RFMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("RFMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
-        ("CKDMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("CKDMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
+    [   (state, GasOpticsRRTMGP( # type: ignore
+                              gas_optics_file = file) )
+         for file in [GasOpticsFiles.LW_G256] \
+         for state in ["RCE_STATES", "RFMIP_STATES", "CKDMIP_STATES"]
     ],
 )
 
@@ -83,25 +65,10 @@ def test_rte_example(example, gas_optics) -> None:
 
 @pytest.mark.parametrize(
     "example, gas_optics",
-    [
-        ("RCE_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("RCE_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
-        ("RFMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("RFMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
-        ("CKDMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.LW_G256,)
-        ),
-        ("CKDMIP_STATES", GasOpticsRRTMGP( # type: ignore
-                              gas_optics_file = GasOpticsFiles.SW_G112,)
-        ),
+    [   (state, GasOpticsRRTMGP( # type: ignore
+                              gas_optics_file = file) )
+         for file in [GasOpticsFiles.LW_G256] \
+         for state in ["RCE_STATES", "RFMIP_STATES", "CKDMIP_STATES"]
     ],
 )
 def test_rte_example_with_dask(example, gas_optics) -> None:
